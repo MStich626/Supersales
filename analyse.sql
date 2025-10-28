c8e586eab67e713b3412faf2bd83a81a02a3b87a
MStich626 <104522384+MStich626@users.noreply.github.com>
2025-10-28 13:14:12 +0100

    Add new query

--- analyse.sql
+++ analyse.sql
@@ -1,4 +1,4 @@
--- Top 3 products in each catgeory by revenue
+-- Top 3 products in each category by revenue
 -- Business Goal: Identify the best-selling products in each category.
 
 
@@ -30,4 +30,39 @@ SELECT
 	ROUND((CTE.revenue * 100) / REV_BY_CATEGORY.SUM_BY_CATEGORY::NUMERIC, 2) AS PERCENTAGE 
 FROM CTE 
 JOIN REV_BY_CATEGORY ON CTE."category" = REV_BY_CATEGORY."category" 
-WHERE CTE.POSITION <= 3
+WHERE CTE.POSITION <= 3;
+
+
+
+
+-- Most return products
+-- Business Goal: Identify which products are most returned
+
+
+WITH MOST_RETURN_PRODUCTS AS
+(
+	SELECT 
+		p.product_name,
+		COUNT(ordr.order_id) AS COUNT_OF_RETURNS
+	FROM customers c JOIN orders ord ON c.customer_id = ord.customer_id
+	JOIN order_positions op ON op.order_id = ord.order_id
+	JOIN products_v2 p ON p.product_id = op.product_id
+	JOIN order_returns ordr ON ordr.order_id = ord.order_id
+	GROUP BY 1
+),
+TOTAL AS
+(
+	SELECT COUNT(order_r.order_id) AS SUM_OF_RETURNS FROM order_returns order_r
+	JOIN order_positions op ON op.order_id = order_r.order_id
+)
+SELECT
+	MOST_RETURN_PRODUCTS.*, 
+	round(((MOST_RETURN_PRODUCTS.COUNT_OF_RETURNS * 100) / TOTAL.SUM_OF_RETURNS::NUMERIC), 2) 
+	FROM MOST_RETURN_PRODUCTS JOIN TOTAL ON 1 = 1
+	ORDER BY 2 DESC;
+
+
+
+
+
+
